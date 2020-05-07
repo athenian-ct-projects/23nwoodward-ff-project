9d6256cef2e0fe1538855202eb6cef77d98121c0
23nwoodward <23nwoodward@athenian.org>
2020-05-07 16:41:17 +0000

    started code

--- focus_day_project.py
+++ focus_day_project.py
@@ -1,3 +1,12 @@
-# Also, be sure to use comments throughout your program. Use good programming practices, including organization, documentation and citation. Yes, you need to cite your sources! (You can do so using comments at the bottom of your code.)
+
 # This game is for Risky Buisness Day by NW '23
-test
+# Make sure to get enough money to stay in business, but enough sugar to keep up tour energy thrughout the day!
+
+cost = float(input("How much are you selling your item/experience for today?"))
+money = cost*3
+if cost > 0: 
+    print("great, let's get started! You start with &" + money + ".")
+input1 = int(input("Do you want to (1) help start setting up the business, or (2) scope out other businesses? pick 1 or 2. "))
+if input1 == 1:
+    money = money + cost 
+    input11 = int(input("Congrats, "))
